6c63f9f44bedb64e9639464ebcb86f9a260b02cb
jalbury <jalbury@knights.ucf.edu>
2019-04-13 10:51:37 -0400

    added performance test for STM implementation

--- stm_vector.cpp
+++ stm_vector.cpp
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <chrono>
 #include <iostream>
 #include <signal.h>
 #include <pthread.h>
@@ -11,8 +12,17 @@
 #include <cmath>
 using namespace std;
 
+// constant across all tests
+const int NUM_TRANSACTIONS = 500000;
+const int NUM_OP_LOOPS = NUM_TRANSACTIONS / 100;
+
+// vary these for performance tests
 const int THREAD_COUNT = 4;
-const int NUM_TRANSACTIONS = 100000;
+const int TRANSACTION_SIZE = 1;
+const int PERCENT_PUSHBACK = 50;
+const int PERCENT_POPBACK = 50;
+const int PERCENT_READ = 0;
+const int PERCENT_WRITE = 0;
 
 Config::Config() :
     bmname(""),
@@ -24,7 +34,7 @@ Config::Config() :
     lookpct(34),
     inspct(66),
     sets(1),
-    ops(1),
+    ops(TRANSACTION_SIZE),
     time(0),
     running(true),
     txcount(0)
@@ -37,34 +47,41 @@ void* run_thread(void* arg) {
     // each thread must be initialized before running transactions
     TM_THREAD_INIT();
 
+    // cast argument as a pointer to shared STM vector object
     stm_vector *vec = reinterpret_cast<stm_vector*>(arg);
-    int val;
-    for (int i=0; i<5; i++) {
-	    vec->pushback(i);
+
+    // execute NUM_TRANSACTIONS total operations on the vector
+    int i, j;
+    for (i=0; i<NUM_OP_LOOPS; i++) {
+	    // execute pushback operations
+	    for (j=0; j<PERCENT_PUSHBACK; j++)
+		    vec->pushback(j);
+	    // execute read operations
+	    for (j=0; j<PERCENT_READ; j++)
+		    vec->read(j);
+	    // execute write operations
+	    for (j=0; j<PERCENT_WRITE; j++)
+		    vec->write(j, 0);
+	    // execute popback operations
+	    for (j=0; j<PERCENT_POPBACK; j++)
+		    vec->popback();
     }
-    vec->popback();
-    vec->write(2, 12345);
 
-//    for(int i=0; i<NUM_TRANSACTIONS; i++) {
-//      vec->pushback(i);
-//      val = vec->popback();
-//    }
-
+    // shutdown thread when done
     TM_THREAD_SHUTDOWN();
 }
 
 int main(int argc, char** argv) {
     TM_SYS_INIT();
 
+    // get start time of execution
+    auto start = chrono::high_resolution_clock::now();
     // original thread must be initalized also
     TM_THREAD_INIT();
+
+    // create and initialize shared vector
     stm_vector vec;
     vec.initialize(0);
-
-    int size = vec.size(), capacity = vec.capacity();
-    std::cout << "after initialization\n";
-    std::cout << "size: " + std::to_string(size) + "\n";
-    std::cout << "capacity: " + std::to_string(capacity) + "\n";
 
     void* args[256];
     pthread_t tid[256];
@@ -88,27 +105,17 @@ int main(int argc, char** argv) {
     for (uint32_t k = 1; k < CFG.threads; k++)
         pthread_join(tid[k], NULL);
 
-    size = vec.size(), capacity = vec.capacity();
-    std::cout << "after threads finish\n";
-    std::cout << "size: " + std::to_string(size) + "\n";
-    std::cout << "capacity: " + std::to_string(capacity) + "\n";
+    // get end time of exection
+    auto end = chrono::high_resolution_clock::now();
+    // calculate elapsed execution time
+    chrono::duration<double> elapsed = end - start;
 
-    std::cout << "reading all elements...\n";
-    int val;
-    for (int i=0; i<size; i++) {
-	    val = vec.read(i);
-	    std::cout << to_string(val) + " ";
-    }
-    std::cout << std::endl;
-    std::cout << "popping all elements...\n";
-    for (int i=0; i<size; i++) {
-	    val = vec.popback();
-	    std::cout << to_string(val) + " ";
-    }
-    std::cout << std::endl;
-    size = vec.size(), capacity = vec.capacity();
-    std::cout << "size: " + std::to_string(size) + "\n";
-    std::cout << "capacity: " + std::to_string(capacity) + "\n";
+    // print results
+    cout << "Number of threads: " << THREAD_COUNT << "\n";
+    cout << "Transaction size: " << TRANSACTION_SIZE << "\n";
+    cout << "Op ratio: " << PERCENT_PUSHBACK << "\% pushback, " << PERCENT_POPBACK << "\% popback, " 
+	    << PERCENT_READ << "\% read, " << PERCENT_WRITE << "\% write\n";
+    cout << "Total elapsed time: " << elapsed.count() << " seconds\n";
 
     // And call sys shutdown stuff
     TM_SYS_SHUTDOWN();
@@ -125,15 +132,18 @@ void stm_vector::initialize() {
 void stm_vector::initialize(int n) {
 	int max_capacity, capacity, size;
 	TM_BEGIN(atomic) {
+		// calculate max capacity of vector and initalize size and capacity to zero
 		max_capacity = pow(2, NUM_LEVELS + 1) - 2;
 		capacity = 0;
-    size = 0;
+		size = 0;
 		TM_WRITE(_size, size);
 		TM_WRITE(_capacity, capacity);
 		TM_WRITE(_max_capacity, max_capacity);
+		// allocate memory for the first level of the array that represents the vector
 		TM_WRITE(array, (node_t **)TM_ALLOC(NUM_LEVELS * sizeof(node_t *)));
 	}
 	TM_END;
+	// reserve space in the vector for n elements
 	stm_vector::reserve(n);
 }
 
@@ -147,7 +157,7 @@ void stm_vector::pushback(int val) {
 		// if we don't need to increase capacity, write val to end of vector and update size
 		if (size < capacity) {
 			TM_WRITE(array[get_bucket(size)][get_idx_within_bucket(size)].val, val);
-      TM_WRITE(_size, size+1);
+			TM_WRITE(_size, size+1);
 			reserve_space = 0;
 		}
 		// otherwise, we'll need to reserve space before completing operation
@@ -157,13 +167,15 @@ void stm_vector::pushback(int val) {
 	}
 	TM_END;
 
+	// if we already completed the operation, simply return
 	if (!reserve_space)
 		return;
 
+	// otherwise, reserve more space and complete operation
 	stm_vector::reserve(size+1);
 	TM_BEGIN(atomic) {
 		TM_WRITE(array[get_bucket(size)][get_idx_within_bucket(size)].val, val);
-    TM_WRITE(_size, size+1);
+		TM_WRITE(_size, size+1);
 	}
 	TM_END;
 }
@@ -172,7 +184,7 @@ void stm_vector::pushback(int val) {
 int stm_vector::popback() {
 	int size, retval;
 	TM_BEGIN(atomic) {
-		// get current value of size
+		// get current value of size and decrement
 		size = TM_READ(_size) - 1;
 		// store value at end of vector and update size
 		retval = TM_READ(array[get_bucket(size)][get_idx_within_bucket(size)].val);
@@ -229,11 +241,16 @@ void stm_vector::reserve(int n) {
 
 // returns the value at the given index (if the index is valid)
 int stm_vector::read(int idx) {
-	int size, retval, i = get_bucket(idx), j = get_idx_within_bucket(idx);
+	int size, retval;
+	// calculate indices within two-level array
+	int i = get_bucket(idx), j = get_idx_within_bucket(idx);
 
 	TM_BEGIN(atomic) {
+		// get current size of value
 		size = TM_READ(_size);
+		// check that given index is within bounds
 		if (idx >= size) throw std::out_of_range("read() with index " + std::to_string(idx) + " out of bounds.");
+		// get value at the given index
 		retval = TM_READ(array[i][j].val);
 	}
 	TM_END;
@@ -243,11 +260,16 @@ int stm_vector::read(int idx) {
 
 // writes the given value to the given index (if the index is valid)
 void stm_vector::write(int idx, int val) {
-	int size, i = get_bucket(idx), j = get_idx_within_bucket(idx);
+	int size;
+	// calculate indices within two-level array
+	int i = get_bucket(idx), j = get_idx_within_bucket(idx);
 
 	TM_BEGIN(atomic) {
+		// get current value of size
 		size = TM_READ(_size);
+		// check that given index is within bounds 
 		if (idx >= size) throw std::out_of_range("write() with index " + std::to_string(idx) + " out of bounds.");
+		// write given value at given index
 		TM_WRITE(array[i][j].val, val);
 	}
 	TM_END;
@@ -258,6 +280,7 @@ int stm_vector::size() {
 	int retval;
 
 	TM_BEGIN(atomic) {
+		// get current size of vector
 		retval = TM_READ(_size);
 	}
 	TM_END;
@@ -270,6 +293,7 @@ int stm_vector::capacity() {
 	int retval;
 
 	TM_BEGIN(atomic) {
+		// get current capacity of vector
 		retval = TM_READ(_capacity);
 	}
 	TM_END;
@@ -279,9 +303,11 @@ int stm_vector::capacity() {
 
 // allocate a bucket at the given index of the two-level array
 void stm_vector::allocate_bucket(int bucketIdx) {
+	// calculate size of bucket for given index
 	int bucketSize = 1 << (bucketIdx + stm_vector::highest_bit(FBS));
 
 	TM_BEGIN(atomic) {
+		// allocate bucket at the given index
 		TM_WRITE(array[bucketIdx], (node_t *)TM_ALLOC(bucketSize * sizeof(node_t)));
 	}
 	TM_END;
