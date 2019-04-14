d813dc6e93cbf1b78a1eeec7c692d2f196e92493
Dax <daxborde@knights.ucf.edu>
2019-04-13 20:36:23 -0400

    Added groundwork for comb_vec: .h and copy methods
    Changes and additions to parallel_vector.h, including the vector class,
    the type of vector data, and some constants.
    Also added methods to parallel_vector.cpp that are unchanged from the
    sequential vector.

--- parallel_vector.cpp
+++ parallel_vector.cpp
@@ -5,3 +5,43 @@ int main(int argc, char **argv)
 	printf("hello world\n");
 	return 0; 
 }
+
+// returns the index of the bucket for i (level zero of the array).
+int comb_vector::get_bucket(int i) {
+	int pos = i + FBS;
+	int hiBit = comb_vector::highest_bit(pos);
+	return hiBit - comb_vector::highest_bit(FBS);
+}
+// returns the index within the bucket for i (level one of the array).
+int comb_vector::get_idx_within_bucket(int i) {
+	int pos = i + FBS;
+	int hiBit = comb_vector::highest_bit(pos);
+	return pos ^ (1 << hiBit);
+}
+
+// returns the index of the highest one bit. eg. highest_bit(8) = 3
+int comb_vector::highest_bit(int n) {
+	return comb_vector::number_of_trailing_zeros(comb_vector::highest_one_bit(n));
+}
+
+// from source code for Java Integer class
+int comb_vector::number_of_trailing_zeros(unsigned int i) {
+	int y;
+	if (i == 0) return 32;
+	int n = 31;
+	y = i <<16; if (y != 0) { n = n -16; i = y; }
+	y = i << 8; if (y != 0) { n = n - 8; i = y; }
+	y = i << 4; if (y != 0) { n = n - 4; i = y; }
+	y = i << 2; if (y != 0) { n = n - 2; i = y; }
+	return n - ((i << 1) >> 31);
+}
+
+// from source code for Java Integer class
+int comb_vector::highest_one_bit(unsigned int i) {
+	i |= (i >>  1);
+	i |= (i >>  2);
+	i |= (i >>  4);
+	i |= (i >>  8);
+	i |= (i >> 16);
+	return i - (i >> 1);
+}
--- parallel_vector.h
+++ parallel_vector.h
@@ -1,23 +1,50 @@
 #include <cstdlib>
 #include <cstdio>
+#include <atomic>
+#include <vector>
+#include <array>
 
 // We'll have to decide on a maximum queue size
 #define QSize 100000
+#define NUM_LEVELS 30
+#define FBS 2
+
+class comb_vector {
+	private:
+		Vector global_vector;
+		int size, capacity, max_capacity;
+	public:
+		comb_vector();
+		comb_vector(int n);
+		void pushback(int val);
+		int popback();
+		void reserve(int n);
+		int read(int idx);
+		void write(int idx, int val);
+		int size();
+		int capacity();
+    void allocate_bucket(int bucketIdx);
+    int get_bucket(int i);
+    int get_idx_within_bucket(int i);
+    int highest_bit(int n);
+    int number_of_trailing_zeros(unsigned int i);
+    int highest_one_bit(unsigned int i);
+};
 
 typedef enum OpType_t {
 	PUSH,
 	POP
 } OpType;
 
-typedef struct Node_t {
-	int value;
-} Node;
+// typedef struct Node_t {
+// 	int value;
+// } Node;
 
 typedef struct WriteDescr_t {
 	bool pending;
 	size_t pos;
-	Node *v_old;
-	Node *v_new;
+	int v_old;
+	int v_new;
 } WriteDescr;
 
 typedef struct Queue_t {
@@ -36,7 +63,7 @@ typedef struct Descr_t {
 } Descr;
 
 typedef struct Vector_t {
-	Node **vdata;
+	std::array<std::atomic<std::vector<std::atomic_int>*>, NUM_LEVELS> vdata;
 	Descr *descr;
 	Queue *batch;
 } Vector;
@@ -45,5 +72,5 @@ typedef struct th_info_t {
 	Vector *root;
 	Queue *q;
 	size_t offset;
-	Queue batch;
+	Queue *batch;
 } th_info;
