9e840e3d80e2e1659509892dfd21dcbc08bc1a3d
Dax <daxborde@knights.ucf.edu>
2019-04-15 14:37:36 -0400

    Add pushback, more groundwork, see detailed msg Implemented pushback as seen in the java version. Matt and I realized it may not be entirely correct, so more changes to come.
    Changed various struct types to classes so they can have constructors
    and generally conform to object-oriented design.
    
    Determined a datatype for vdata. It uses a vector, but the size of this
    vector should never be changed, only set once at initialization. I've
    also swapped to using atomic integers instead of references that are
    atomic. The minimum value of an integer shall be reserved for marking
    as logically deleted.
    
    Removed typedef from enum b/c that is not required in C++.
    
    The groundwork was creating method skeletons for the many other
    methods that need implementing, though we still need to make
    constructors for many of the classes.

--- parallel_vector.cpp
+++ parallel_vector.cpp
@@ -3,8 +3,145 @@
 int main(int argc, char **argv)
 {
 	printf("hello world\n");
-	return 0; 
+	return 0;
 }
+
+comb_vector::comb_vector() {
+
+}
+
+comb_vector::comb_vector(int n) {
+
+}
+
+int comb_vector::popback() {
+	return 0;
+}
+
+void comb_vector::pushback(int val) {
+	bool will_add_to_batch = false, help = false;
+	descr *curr_d = nullptr, *new_d = nullptr;
+	while(true) {
+		// POSSIBLE MEMORY LEAK WITH CURR_D
+		curr_d = global_vector->vector_desc.load();
+
+		// Complete any pending operation.
+		comb_vector::complete_write(curr_d->opdesc);
+
+		// If there's a current or ready Combine operation, this thread will 
+		// help complete it.
+		if(curr_d->batch != nullptr) {
+			comb_vector::combine(curr_d, true);
+		}
+
+		// Determine which bucket this element will go in.
+		int bucket_idx = get_bucket(curr_d->size);
+		// If the appropriate bucket doesn't exist, create it.
+		if (global_vector->vdata[bucket_idx] == nullptr) 
+			comb_vector::allocate_bucket(bucket_idx);
+		// Create a new Descriptor and WriteDescriptor.
+		write_descr *writeop = 
+			new write_descr(read_ref_at(curr_d->size), val, curr_d->size);
+		// writeOp->pending = true;
+		// writeOp->v_old = ;
+		// writeOp->v_new = val;
+		// writeOp->pos = curr_d->size;
+
+		new_d = new descr(curr_d->size + 1, writeop, op_type::PUSH);
+
+		// If our CAS failed (in a previous loop iteration) or this thread 
+		// has already added items to the queue, then we'll try to add this 
+		// operation to the queue. (Once we add one operation to the queue, 
+		//we'll keep doing so until that queue closes.)
+		if (will_add_to_batch) { 
+			//|| (threadInfo.q != null && threadInfo.q == batch.get())) {
+			if(add_to_batch(new_d)) {
+				return; // The operation was added to the queue
+			}
+
+			// We couldn't add it to the queue.
+			// [[Not sure why we set newDesc.writeOp to null - 
+			// we didn't add it to the queue, so where did it go?]]
+			free(new_d);
+			new_d = new descr(curr_d->size, nullptr, NONE);
+			help = true;
+			//threadInfo.q = null;
+		}
+
+		// Try the normal compare and set.
+		if (global_vector->vector_desc.compare_exchange_strong(curr_d, new_d)) {
+			if (new_d->batch != nullptr) {
+				// AddToBatch set the descriptor's queue, which only happens 
+				// when we're ready to combine.
+				combine(new_d, true);
+				if (help) { // [[We're going to help another thread, I guess?]]
+					help = false;
+					continue;
+				}
+			}
+			break; // We're done.
+		} else {
+			// The thread adds the operation to the combining queue 
+			// (in the next loop iteration).
+			will_add_to_batch = true;
+		}
+	}
+
+	complete_write(new_d->opdesc);
+	//threadInfo.size = newDesc.size;
+}
+
+void comb_vector::reserve(int n) {
+
+}
+
+int comb_vector::read(int idx) {
+	return 0;
+}
+
+void comb_vector::write(int idx, int val) {
+	
+}
+
+int comb_vector::get_size() {
+	return 0;
+}
+
+int comb_vector::get_capacity() {
+	return 0;
+}
+
+/////////////////////////////////
+
+bool comb_vector::add_to_batch(descr *d){
+	return false;
+}
+
+int comb_vector::combine(descr *d, bool dont_need_to_return) {
+	return 0;
+}
+
+bool comb_vector::inbounds(int idx) {
+	return false;
+}
+
+void comb_vector::marknode(int idx) {
+
+}
+
+void comb_vector::complete_write(write_descr writeop) {
+
+}
+
+void comb_vector::allocate_bucket(int bucketIdx) {
+
+}
+
+int comb_vector::read_ref_at(int idx) {
+	return 0;
+}
+
+/////////////////////////////////
 
 // returns the index of the bucket for i (level zero of the array).
 int comb_vector::get_bucket(int i) {
@@ -21,7 +158,8 @@ int comb_vector::get_idx_within_bucket(int i) {
 
 // returns the index of the highest one bit. eg. highest_bit(8) = 3
 int comb_vector::highest_bit(int n) {
-	return comb_vector::number_of_trailing_zeros(comb_vector::highest_one_bit(n));
+	int temp = comb_vector::highest_one_bit(n);
+	return comb_vector::number_of_trailing_zeros(temp);
 }
 
 // from source code for Java Integer class
@@ -44,4 +182,11 @@ int comb_vector::highest_one_bit(unsigned int i) {
 	i |= (i >>  8);
 	i |= (i >> 16);
 	return i - (i >> 1);
+}
+
+write_descr::write_descr(int o, int n, int p) {
+	pending = true;
+	v_old = o;
+	v_new = n;
+	pos = p;
 }
--- parallel_vector.h
+++ parallel_vector.h
@@ -9,43 +9,20 @@
 #define NUM_LEVELS 30
 #define FBS 2
 
-class comb_vector {
-	private:
-		Vector global_vector;
-		int size, capacity, max_capacity;
-	public:
-		comb_vector();
-		comb_vector(int n);
-		void pushback(int val);
-		int popback();
-		void reserve(int n);
-		int read(int idx);
-		void write(int idx, int val);
-		int size();
-		int capacity();
-    void allocate_bucket(int bucketIdx);
-    int get_bucket(int i);
-    int get_idx_within_bucket(int i);
-    int highest_bit(int n);
-    int number_of_trailing_zeros(unsigned int i);
-    int highest_one_bit(unsigned int i);
+enum op_type {
+	PUSH,
+	POP,
+	NONE
 };
 
-typedef enum OpType_t {
-	PUSH,
-	POP
-} OpType;
-
-// typedef struct Node_t {
-// 	int value;
-// } Node;
-
-typedef struct WriteDescr_t {
-	bool pending;
-	size_t pos;
-	int v_old;
-	int v_new;
-} WriteDescr;
+class write_descr {
+	public:
+		bool pending;
+		size_t pos;
+		int v_old;
+		int v_new;
+		write_descr(int o, int n, int p);
+};
 
 typedef struct Queue_t {
 	bool closed;
@@ -54,23 +31,58 @@ typedef struct Queue_t {
 	size_t head;
 } Queue;	
 
-typedef struct Descr_t {
-	WriteDescr *opDesc;
-	size_t size;
-	size_t offset;
-	Queue *batch;
-	OpType op;
-} Descr;
+class descr {
+	public:
+		write_descr opdesc;
+		size_t size;
+		size_t offset;
+		Queue *batch;
+		op_type op;
+		descr(int s, write_descr *wd, op_type );
+};
 
-typedef struct Vector_t {
-	std::array<std::atomic<std::vector<std::atomic_int>*>, NUM_LEVELS> vdata;
-	Descr *descr;
-	Queue *batch;
-} Vector;
+class vector_vars {
+	public:
+		std::array<std::atomic<std::vector<std::atomic_int>*>, NUM_LEVELS> vdata;
+		std::atomic<descr *> vector_desc;
+		std::atomic<Queue *> batch;
+		vector_vars();
+};
 
+// Still not entirely sure how thread locals work, so change this if needed
 typedef struct th_info_t {
-	Vector *root;
+	//vector_vars root;
 	Queue *q;
 	size_t offset;
 	Queue *batch;
 } th_info;
+
+class comb_vector {
+	private:
+		vector_vars *global_vector;
+		int size, capacity, max_capacity;
+
+		bool add_to_batch(descr *d);
+		int combine(descr *d, bool dont_need_to_return);
+		bool inbounds(int idx);
+		void marknode(int idx);
+		void complete_write(write_descr writeop);
+		void allocate_bucket(int bucketIdx);
+		int read_ref_at(int idx);
+
+		int get_bucket(int i);
+		int get_idx_within_bucket(int i);
+		int highest_bit(int n);
+		int number_of_trailing_zeros(unsigned int i);
+		int highest_one_bit(unsigned int i);
+	public:
+		comb_vector();
+		comb_vector(int n);
+		void pushback(int val);
+		int popback();
+		void reserve(int n);
+		int read(int idx);
+		void write(int idx, int val);
+		int get_size();
+		int get_capacity();
+};
